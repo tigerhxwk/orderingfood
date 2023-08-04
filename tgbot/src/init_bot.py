10bf2603830bcff5805d0d2ff70ab77268703c03
tigerhxwk <stxrmdx@icloud.com>
2023-08-04 17:18:43 +0700

    [tg_bot] Fixed runtime errors in bot initializer

--- tgbot/src/init_bot.py
+++ tgbot/src/init_bot.py
@@ -4,18 +4,20 @@ import logging, os
 
 API_KEY_FILE = os.getcwd() + "/tgbot/http_api.key"
 
-try:
-    keyfile = open(API_KEY_FILE, "r")
-except FileNotFoundError:
-    logger.debug ("file does not exist")
-    os.exit(1)
-
-foodBot = Bot(keyfile.read().replace('\n', ''))
-foodBotDispatcher = Dispatcher(foodBot)
-
 logger = logging.getLogger("actionlogger")
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(f"{__name__}.log", mode='w')
 formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
+
+try:
+    keyfile = open(API_KEY_FILE, "r")
+except FileNotFoundError:
+    logger.debug ("key file does not exist")
+    # exit is a built-in function
+    exit(1)
+
+foodBot = Bot(keyfile.read().replace('\n', ''))
+foodBotDispatcher = Dispatcher(foodBot)
+
