160af5630cc8bca9bd9fa9b61cc911e43c8e6bda
tigerhxwk <95970443+tigerhxwk@users.noreply.github.com>
2023-08-04 17:04:05 +0700

    Revert "Parser"

--- tgbot/src/menu/buttons_builder.py
+++ tgbot/src/menu/buttons_builder.py
@@ -2,13 +2,12 @@ from aiogram import types
 from init_bot import logger
 
 def create_categories_buttons (Menu : dict ()):
-    categories = Menu.keys()
-
+    categories = Menu['category']
     menuCategories = []
     for category in categories:
         menuCategories.append(types.InlineKeyboardButton(category,
-                                                          callback_data=f"category_{category}"))
-        logger.debug (f"created menu button {category} and callback category_{category}")
+                                                          callback_data=f"category_{categories.index(category)}"))
+        logger.debug (f"created menu button {category} and callback category_{categories.index(category)}")
 
     menuButtons = types.InlineKeyboardMarkup(row_width=2)
     for button in menuCategories:
@@ -17,5 +16,4 @@ def create_categories_buttons (Menu : dict ()):
     menuButtons.row()
     menuButtons.insert(types.InlineKeyboardButton("Ничего", callback_data="discard"))
     logger.debug(f"{create_categories_buttons.__name__} finished")
-
     return menuButtons
